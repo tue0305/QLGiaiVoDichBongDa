0045883f93bfc10431921a4d7bfb2ed0ee2136ff
tue0305 <49754302+tue0305@users.noreply.github.com>
2020-09-19 13:11:35 +0700

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.secret_key = "b'\xdc\xec\xe5\xa393\xba\x0e\xf3\xe7\x8d-A\x1c6\xae'"
 
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:28101999@localhost/football_league?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:8774165@localhost/football_league?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app=app)
